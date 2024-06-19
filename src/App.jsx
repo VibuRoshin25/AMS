fa6501dbb9f89405863f4d593b6ece9a72621e09
Vino Kishore <vinokishorefp@gmail.com>
2024-06-20 04:55:50 +0530

    user-card-created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import UserProfile from "./components/Userprofile";
-
-
 function App() {
   return (
     <>
       <h1 className="underline m-10 text-cyan-600 border ">
         Flipopay AMS Application
       </h1>
-      <UserProfile />
     </>
- 
   );
 }
 
