c5f343cdca4ada7a7024270119a883d6de3d95e1
Vino Kishore <vinokishorefp@gmail.com>
2024-06-20 02:23:24 +0530

    feat/4-user-profile: Create user profile.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import EditModal from "./components/EditModal";
+import UserProfile from "./components/Userprofile";
+
 
 function App() {
   return (
@@ -6,7 +7,9 @@ function App() {
       <h1 className="underline m-10 text-cyan-600 border ">
         Flipopay AMS Application
       </h1>
+      <UserProfile />
     </>
+ 
   );
 }
 
