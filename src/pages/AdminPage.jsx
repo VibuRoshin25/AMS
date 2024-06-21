870a93f37eabb02282d1ac9b3394cd0ecf8c2dd5
Vibu Roshin E V <viburoshin57@gmail.com>
2024-06-21 11:46:43 +0530

    modified:   src/pages/AdminPage.jsx

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import StyledDatePicker from "../components/StyledDatePicker";
 import SearchBar from "../components/SearchBar";
-import Recordstable from "../components/Recordstable";
+import Recordstable from "../components/RecordsTable";
 
 export default function AdminPage() {
   return (
