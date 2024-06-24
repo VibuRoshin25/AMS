4b0d833c6f73a73d26750610b5506925284e3365
suhail <shaikmsuhail02@gmail.com>
2024-06-24 14:14:11 +0530

    feat/9-attendance-collection:firestore attendance collection setup done

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2,30 +2,22 @@ import Header from "../components/Header";
 import StyledDatePicker from "../components/StyledDatePicker";
 import SearchBar from "../components/SearchBar";
 import Recordstable from "../components/Recordstable";
-<<<<<<< HEAD
-=======
 import CreateEmployeeModal from "../components/admin/CreateEmployeeModal";
->>>>>>> dev
 
 export default function AdminPage() {
-  
   return (
-    
-
-   <div>
-    <div className="relative">
-      
-      <div className="flex flex-col justify-center items-center backdrop-blur-xl relative ">
-        <Header />
-        <div className="flex justify-center gap-20 mt-8">
-          <StyledDatePicker />
-          <SearchBar />
-          <CreateEmployeeModal />
+    <div>
+      <div className="relative">
+        <div className="flex flex-col justify-center items-center backdrop-blur-xl relative ">
+          <Header />
+          <div className="flex justify-center gap-20 mt-8">
+            <StyledDatePicker />
+            <SearchBar />
+            <CreateEmployeeModal />
+          </div>
+          <Recordstable />
         </div>
-        <Recordstable />
       </div>
     </div>
-    </div>
-
   );
 }
